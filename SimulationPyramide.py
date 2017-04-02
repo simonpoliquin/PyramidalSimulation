7c21bc60aac3ae152d7afe37b2af4a4453a51d37
Simon Poliquin <simonpoliquin@hotmail.com>
2017-04-02 16:13:13 -0400

    Refactoring of PyramidalSimulation as a object

--- SimulationPyramide.py
+++ SimulationPyramide.py
@@ -1,39 +1,93 @@
 from People import *
 from collections import deque
-ni = 10 # number of conversion minimum by turn
-NumberOfPeopleConverted = 0
-NumberOfConversion = NumberOfPeopleConverted + ni # equation of the number of people
-NumberOfPeople = 1000000 # Number ofr the people in the simulation
-
-NumberOfCycle = 100
-
-averageValueConversionCoefficient = 0.5  # average value taken by the coefficient
-standardDeviationConversionCoefficient = 0.2  # standard deviation of the coefficient
-
-averageValueLeavingCoefficient = 0.5  # average value taken by the coefficient
-standardDeviationLeavingCoefficient = 0.2  # sandard deviation of the coefficient
-
-peopleUntouched = deque()
-[peopleUntouched.append(People()) for i in range(NumberOfPeople)]
-for people in peopleUntouched:
-    people.makePeople(averageValueConversionCoefficient, standardDeviationConversionCoefficient ,averageValueLeavingCoefficient ,standardDeviationLeavingCoefficient)
+import math
 
 
-peopleConverted = deque()
-peopleNotConverted = deque()
-for i in range (0,NumberOfCycle):
-    for j in range (0,NumberOfConversion):
-        peopleUntouched[0].Conversion()
-        if  peopleUntouched[0].isConverted :
-            peopleConverted.append(peopleUntouched[0])
+class PyramidalSimulation(object):
+
+
+    ni = 10  # number of conversion minimum by turn
+    NumberOfPeopleConverted = 0
+    NumberOfPeople = 1000000  # Number ofr the people in the simulation
+
+    NumberOfCycle = 100
+
+    averageValueConversionCoefficient = 0.5  # average value taken by the coefficient
+    standardDeviationConversionCoefficient = 0.2  # standard deviation of the coefficient
+
+    averageValueLeavingCoefficient = 0.5  # average value taken by the coefficient
+    standardDeviationLeavingCoefficient = 0.2  # sandard deviation of the coefficient
+
+    lossCoefficient = 0.1
+    ConversionCoefficient = 0.5
+
+
+    peopleUntouched = deque()
+
+    peopleConverted = deque()
+    peopleNotConverted = deque()
+
+
+    def __init_(self):
+        [self.peopleUntouched.append(People()) for i in range(self.NumberOfPeople)] #Create the list with the people
+        for people in self.peopleUntouched: #Initialise the person in the list
+            people.makePeople(self.averageValueConversionCoefficient, self.standardDeviationConversionCoefficient,
+                              self.averageValueLeavingCoefficient, self.standardDeviationLeavingCoefficient)
+
+    def Conversion(self):
+
+        NumberOfConversionAttempt = self.NumberOfConversion()
+        for j in range(0, NumberOfConversionAttempt):
+            self.peopleUntouched[0].Conversion() # Attemp to Conversion
+            self.PlaceInGoodList(self.peopleUntouched[0]) #following the result place the person in the good list
+            self.peopleUntouched.popleft() # remove the person from the list of person untouched.
+
+
+    def PlaceInGoodList(self,people):
+        if self.people.isConverted:
+            people.append(self.peopleUntouched[0])
         else:
-            peopleNotConverted.append(peopleUntouched[0])
-        peopleUntouched.popleft()
+            people.append(self.peopleUntouched[0])
 
-    NumberOfPeopleConverted = len(peopleConverted)
-    print(NumberOfPeopleConverted)
-    NumberOfConversion = NumberOfPeopleConverted + ni
-    print('cycle:',i+1)
+
+
+    def NumberOfConversion(self):
+
+
+        self.numberOfPeopleConverted = len(self.peopleConverted) # Number of people in the list equal the number of people in the pyramdial scheme
+
+        numberOfConversion = math.floor(self.ConversionCoefficient*self.NumberOfPeopleConverted) + self.ni # number of maximal conversion attempt.
+        NumberOfPeopleLeftToConverted = len(self.peopleUntouched)
+        if numberOfConversion > NumberOfPeopleLeftToConverted:
+            return NumberOfPeopleLeftToConverted #  There is less people to converte than the number of people making a attempt
+        else:
+            return numberOfConversion # standar case follow the equation above
+
+    def Leaving(self):
+        NumberOfLeavingAttempt = self.NumberOfLeaving()
+        for j in range(0, NumberOfLeavingAttempt):
+            self.peopleConverted[0].Conversion() # Attemp to Conversion
+            self.PlaceInGoodList(self.peopleConverted[0]) #following the result place the person in the good list
+            self.peopleConverted.popleft() # remove the person from the list of person untouched.
+
+    def NumberOfLeaving(self):
+
+        self.numberOfPeopleConverted = len(self.peopleConverted) # Number of people in the list equal the number of people in the pyramdial scheme
+
+        numberOfPeopleLeaving = math.floor(self.lossCoefficient * self.NumberOfPeopleConverted) + self.ni # number of maximal people thinking leaving
+
+        if numberOfPeopleLeaving > self.numberOfPeopleConverted :
+            return self.numberOfPeopleConverted #  There is less people who can leave than the number of people converted
+        else:
+            return numberOfPeopleLeaving # standar case follow the equation above
+
+
+    def PymarimalSimulation(self):
+        for i in range (0,self.NumberOfCycle):
+            self.Conversion()
+            self.Leaving()
+
+
 
 
 
